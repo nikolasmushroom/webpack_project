e44f540c2067efbcd692b9ce7c2fed3eee2b93bb
nikolasmushroom <dashagribok0905@gmail.com>
2025-01-17 18:00:53 +0300

    feat: were added, svg, asset, babel and css with modules loaders

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,25 +1,75 @@
 import {ModuleOptions} from "webpack";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import {BuildOptions} from "./types/types";
+import ReactRefreshTypeScript from "react-refresh-typescript";
+import {buildBabelLoader} from "./babel/buildBabelLoader";
 
-export function buildLoaders({mode}: BuildOptions): ModuleOptions['rules'] {
-    const isDev = mode === "development";
+export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
+    const isDev = options.mode === "development";
+    const cssLoaderWithModules = {
+        loader: "css-loader",
+        options: {
+            modules: {
+                localIdentName: isDev ? "[path][name]__[local]--[hash:base64:8]" : "[hash:base64:8]",
+            },
+        }
+    }
+    const svgLoader = {
+        test: /\.svg$/,
+        use: [{
+            loader: '@svgr/webpack', options: {
+                icon: true,
+                svgoConfig: {
+                    plugins: [
+                        {
+                            name: 'convertColors',
+                            params: {
+                                currentColor: true
+                            }
+                        }
+                    ]
+                }
+            }
+        }],
+    }
+    const assetLoader = {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+    }
     const scssLoader = {
         test: /\.s[ac]ss$/i,
         use: [isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-            // Translates CSS into CommonJS
-            "css-loader",
-            // Compiles Sass to CSS
-            "sass-loader",
+            cssLoaderWithModules
         ],
     }
+    // const babelLoader = {
+    //     test: /\.tsx?$/,
+    //     exclude: /node_modules/,
+    //     use: {
+    //         loader: "babel-loader",
+    //     }
+    // }
     const tsLoader = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
         exclude: /node_modules/,
+        test: /\.tsx?$/,
+        use: [
+            {
+                loader: 'ts-loader',
+                options: {
+                    getCustomTransformers: () => ({
+                        before: [isDev && ReactRefreshTypeScript()].filter(Boolean),
+                    }),
+                    transpileOnly: true
+                }
+            }
+        ],
     }
+    const babelLoader = buildBabelLoader(options)
     return [
+        assetLoader,
         scssLoader,
-        tsLoader,
+        // tsLoader,
+        babelLoader,
+        svgLoader,
     ]
 }
